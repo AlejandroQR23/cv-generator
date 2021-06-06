7adec7fdcb649277c7ed6fc895c096e6cb62c62d
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2021-06-05 21:39:29 -0500

    preview container component

---
+++ src/components/cv-preview/preview-container/preview-container.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './preview-container.styles.scss';
+
+export class PreviewContainer extends React.Component {
+  //   constructor() {
+  //     super();
+  //   }
+
+  render() {
+    return <div className='col cv-preview mx-2'>CV PREVIEW</div>;
+  }
+}
