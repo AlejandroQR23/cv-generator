2d3ef40f847a9067b138cd1ebbf771a16e104053
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2021-06-05 21:38:47 -0500

    created form container component

---
+++ src/components/cv-form/form-container/form-container.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './form-container.styles.scss';
+
+export class FormContainer extends React.Component {
+  //   constructor() {
+  //     super();
+  //   }
+
+  render() {
+    return <div className='col cv-form mx-2'>CV FORM</div>;
+  }
+}
