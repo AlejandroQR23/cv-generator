ebf3891a1acb2542e5f1a96e481c0a15f6a7bb80
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2021-06-10 21:57:45 -0500

    cv preview finished

---
+++ src/components/cv-preview/preview-body/preview-body.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { PreviewExperience } from './preview-experience/preview-experience.component';
+import { PreviewEducation } from './preview-education/preview-education.component';
+
+export const PreviewBody = ({ education, experience, description }) => (
+  <div className='preview-body col-8 pt-4'>
+    <h5 className='text-dark pb-2'>Description</h5>
+    <p className='mb-2'>{description}</p>
+    <hr />
+    {/* Experience */}
+    <h5 className='text-dark pb-2'>Experience</h5>
+    {experience.map((exp, index) => (
+      <PreviewExperience key={index} experience={exp} />
+    ))}
+    <hr />
+    {/* Education */}
+    <h5 className='text-dark pb-2'>Education</h5>
+    {education.map((ed, index) => (
+      <PreviewEducation key={index} education={ed} />
+    ))}
+  </div>
+);
--- src/components/cv-preview/preview-container/preview-container.component.jsx
+++ src/components/cv-preview/preview-container/preview-container.component.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import './preview-container.styles.scss';
 
+import { PreviewBody } from '../preview-body/preview-body.component';
+import { PreviewHeader } from '../preview-header/preview-header.component';
+import { PreviewSidebar } from '../preview-sidebar/preview-sidebar.component';
+
 export const PreviewContainer = ({ personalInfo, education, experience }) => (
-  <div className='col cv-preview mx-2'>
-    <header>
-      <h1>
-        {personalInfo.firstName} {personalInfo.lastName}
-      </h1>
-      <p>{personalInfo.title}</p>
-    </header>
-    <p>{education.length > 0 ? education[0].schoolName : ''}</p>
+  <div className='col-12 col-xl-5 cv-preview mx-2'>
+    <div className='row'>
+      <PreviewHeader
+        firstName={personalInfo.firstName}
+        lastName={personalInfo.lastName}
+        title={personalInfo.title}
+      />
+    </div>
+    <div className='row content'>
+      <PreviewBody
+        education={education}
+        experience={experience}
+        description={personalInfo.description}
+      />
+      <PreviewSidebar
+        address={personalInfo.address}
+        phoneNumber={personalInfo.phoneNumber}
+        email={personalInfo.email}
+      />
+    </div>
   </div>
 );
