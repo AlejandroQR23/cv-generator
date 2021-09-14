2a405376d99ab1364aa212eb3c3cc40a11918579
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2021-09-13 19:43:54 -0500

    education form now uses redux for managin state

--- src/components/cv-form/education-item/education-item.component.jsx
+++ src/components/cv-form/education-item/education-item.component.jsx
@@ -1,41 +1,78 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  changeEducationInput,
+  deleteEducationItem,
+} from "../../../redux/cv/cv.slice";
 
-const EducationItem = () => (
-  <div className="mt-2">
-    <input
-      type="text"
-      placeholder="School"
-      className="form-control"
-      name="schoolName"
-    />
-    <input
-      type="text"
-      placeholder="City"
-      className="form-control"
-      name="schoolCity"
-    />
-    <input
-      type="text"
-      placeholder="Degree"
-      className="form-control"
-      name="degree"
-    />
-    <input
-      type="text"
-      placeholder="From"
-      className="form-control"
-      name="startDate"
-    />
-    <input
-      type="text"
-      placeholder="To"
-      className="form-control"
-      name="gradDate"
-    />
-    <button className="btn btn-danger mb-1" type="button">
-      Delete Education
-    </button>
-  </div>
-);
+const EducationItem = ({ education, index }) => {
+  const { schoolName, schoolCity, degree, startDate, gradDate } = education;
+  const dispatch = useDispatch();
+
+  return (
+    <div className="mt-2">
+      <input
+        type="text"
+        placeholder="School"
+        className="form-control"
+        name="schoolName"
+        value={schoolName}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeEducationInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="City"
+        className="form-control"
+        name="schoolCity"
+        value={schoolCity}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeEducationInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Degree"
+        className="form-control"
+        name="degree"
+        value={degree}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeEducationInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="From"
+        className="form-control"
+        name="startDate"
+        value={startDate}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeEducationInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="To"
+        className="form-control"
+        name="gradDate"
+        value={gradDate}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeEducationInput({ index, name, value }));
+        }}
+      />
+      <button
+        className="btn btn-danger mb-1"
+        type="button"
+        onClick={() => {
+          dispatch(deleteEducationItem({ index }));
+        }}
+      >
+        Delete Education
+      </button>
+    </div>
+  );
+};
 
 export default EducationItem;
--- src/components/cv-form/education/education.component.jsx
+++ src/components/cv-form/education/education.component.jsx
@@ -1,29 +1,35 @@
+/* eslint-disable react/no-array-index-key */
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addEducationItem, selectEducation } from "../../../redux/cv/cv.slice";
 
-// import { EducationItem } from "../education-item/education-item.component";
+import EducationItem from "../education-item/education-item.component";
 
-const Education = () => (
-  <div className="education mb-2">
-    <h4>Education</h4>
-    {/* {education.map((ed, index) => {
-        return (
-          <EducationItem
-            key={index}
-            education={ed}
-            index={index}
-            changeEd={changeEducation}
-            deleteEd={deleteEducation}
-          />
-        );
-      })} */}
-    <div className="row mt-2">
-      <div className="col">
-        <button className="btn btn-primary" type="button">
-          Add Education
-        </button>
+const Education = () => {
+  const education = useSelector(selectEducation);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="education mb-2">
+      <h4>Education</h4>
+      {education.map((ed, index) => (
+        <EducationItem key={index} education={ed} index={index} />
+      ))}
+      <div className="row mt-2">
+        <div className="col">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => {
+              dispatch(addEducationItem());
+            }}
+          >
+            Add Education
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Education;
