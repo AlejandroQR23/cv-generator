327f27279cf2d31287077880297cc9fd76b52b9d
Alejandro QR <alejandro.quijano@comunidad.unam.mx>
2021-09-13 19:44:06 -0500

    experience form now uses redux for managin state

--- src/components/cv-form/experience-item/experience-item.component.jsx
+++ src/components/cv-form/experience-item/experience-item.component.jsx
@@ -1,40 +1,77 @@
+/* eslint-disable react/prop-types */
 import React from "react";
+import { useDispatch } from "react-redux";
+import {
+  changeExperienceInput,
+  deleteExperienceItem,
+} from "../../../redux/cv/cv.slice";
 
-const ExperienceItem = () => (
-  <div className="mt-2">
-    <input
-      type="text"
-      placeholder="Position"
-      className="form-control"
-      name="position"
-    />
-    <input
-      type="text"
-      placeholder="Company"
-      className="form-control"
-      name="company"
-    />
-    <input
-      type="text"
-      placeholder="City"
-      className="form-control"
-      name="city"
-    />
-    <input
-      type="text"
-      placeholder="From"
-      className="form-control"
-      name="startDate"
-    />
-    <input
-      type="text"
-      placeholder="To"
-      className="form-control"
-      name="endDate"
-    />
-    <button className="btn btn-danger mb-1" type="button">
-      Delete Experience
-    </button>
-  </div>
-);
+const ExperienceItem = ({ experience, index }) => {
+  const { position, company, city, startDate, endDate } = experience;
+  const dispatch = useDispatch();
+
+  return (
+    <div className="mt-2">
+      <input
+        type="text"
+        placeholder="Position"
+        className="form-control"
+        name="position"
+        value={position}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeExperienceInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="Company"
+        className="form-control"
+        name="company"
+        value={company}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeExperienceInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="City"
+        className="form-control"
+        name="city"
+        value={city}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeExperienceInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="From"
+        className="form-control"
+        name="startDate"
+        value={startDate}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeExperienceInput({ index, name, value }));
+        }}
+      />
+      <input
+        type="text"
+        placeholder="To"
+        className="form-control"
+        name="endDate"
+        value={endDate}
+        onChange={({ target: { name, value } }) => {
+          dispatch(changeExperienceInput({ index, name, value }));
+        }}
+      />
+      <button
+        className="btn btn-danger mb-1"
+        type="button"
+        onClick={() => {
+          dispatch(deleteExperienceItem({ index }));
+        }}
+      >
+        Delete Experience
+      </button>
+    </div>
+  );
+};
 export default ExperienceItem;
--- src/components/cv-form/experience/experience.component.jsx
+++ src/components/cv-form/experience/experience.component.jsx
@@ -1,29 +1,38 @@
+/* eslint-disable react/no-array-index-key */
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addExperienceItem,
+  selectExperience,
+} from "../../../redux/cv/cv.slice";
 
-// import { ExperienceItem } from '../experience-item/experience-item.component';
+import ExperienceItem from "../experience-item/experience-item.component";
 
-const Experience = () => (
-  <div className="experience mb-2">
-    <h4>Experience</h4>
-    {/* {experience.map((exp, index) => {
-      return (
-        <ExperienceItem
-          key={index}
-          experience={exp}
-          index={index}
-          changeExp={changeExperience}
-          deleteExp={deleteExperience}
-        />
-      );
-    })} */}
-    <div className="row mt-2">
-      <div className="col">
-        <button className="btn btn-primary" type="button">
-          Add Experience
-        </button>
+const Experience = () => {
+  const experience = useSelector(selectExperience);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="experience mb-2">
+      <h4>Experience</h4>
+      {experience.map((exp, index) => (
+        <ExperienceItem key={index} experience={exp} index={index} />
+      ))}
+      <div className="row mt-2">
+        <div className="col">
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => {
+              dispatch(addExperienceItem());
+            }}
+          >
+            Add Experience
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Experience;
